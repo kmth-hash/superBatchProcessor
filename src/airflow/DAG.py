06c3531fbac6e86043a2e8eab1eeb8e0ba17d7b1
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-06-21 04:53:08 +0530

    DAG Sub tasks updated

--- src/airflow/DAG.py
+++ src/airflow/DAG.py
@@ -1,12 +1,12 @@
-# DAG to create S3 bucket 
 from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.operators.python  import PythonOperator
 from datetime import datetime, timedelta
 import os
+import boto3
 from dotenv import load_dotenv
 load_dotenv('src\.env.dev')
-
 
 default_args = {
     'owner': 'kmth',
@@ -20,14 +20,72 @@ def printTimeStamp(**kwargs ):
     d = datetime.now()
     print(f"{kwargs['params']['processName']} {kwargs['params']['status']} --> {d.strftime('%c')}")
 
+def checkDBExists(**kwargs ): 
+    d = datetime.now() 
+    print(f"{kwargs['params']['processName']} Started --> {d.strftime('%c')}")
+    client = boto3.client('dynamodb')
+    tableName = os.getenv('DYNAMODBTABLE')
+    print(os.getcwd() , tableName , os.getenv('BUCKETREGION'))
+    try:
+        response = client.describe_table(TableName=tableName)
+        # print(response)
+        print('DynamoDB Table exists. ')
+    except client.exceptions.ResourceNotFoundException as ex :
+        print('Table not found. ')
+        response = client.create_table(
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'ID',
+                    'AttributeType': 'S',
+                } 
+                
+            ] , 
+            KeySchema=[
+                {
+                    'AttributeName': 'ID',
+                    'KeyType': 'HASH',
+                } 
+            ] ,
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5,
+            },
+            TableName=tableName,
+        )
+        # print(response)
+    finally : 
+        d = datetime.now() 
+        print(f"{kwargs['params']['processName']} Ended --> {d.strftime('%c')}")
 
-dag = DAG('S3_CREATE_DAG', default_args=default_args, schedule= '@once')
+
+
+dag = DAG('Main_Process_DAG', default_args=default_args, schedule= '@once')
 
 t1 = PythonOperator(
-    task_id='logger-start',
+    task_id='logger-s3-start',
     python_callable=printTimeStamp,
     params={'status' : 'Started' , 'processName' : 'S3 Bucket Creation '},
     dag=dag)
+
+t2 = PythonOperator(
+    task_id='logger-s3-end',
+    python_callable=printTimeStamp,
+    params={'status' : 'Ended' , 'processName' : 'S3 Bucket End '},
+    dag=dag)
+
+t3 = BashOperator(
+    task_id='EndTask' , 
+    bash_command='echo Last task shutting down ==> $(date);' , 
+    output_encoding="utf-8" , 
+    dag=dag
+)
+
+maintask = BashOperator(
+    task_id='FinalTaskInit' , 
+    bash_command='echo Main DAG executed $(date);' , 
+    output_encoding="utf-8" , 
+    dag=dag
+)
 
 create_bucket = S3CreateBucketOperator(
         task_id="create_bucket",
@@ -35,10 +93,13 @@ create_bucket = S3CreateBucketOperator(
         bucket_name=os.getenv('BUCKETID'),
         aws_conn_id='S3_conn',
     )
-t2 = PythonOperator(
-    task_id='logger-end',
-    python_callable=printTimeStamp,
-    params={'status' : 'Ended' , 'processName' : 'S3 Bucket Creation '},
-    dag=dag)
 
+checkDB = PythonOperator(
+    task_id='DB-exists',
+    python_callable=checkDBExists,
+    params={'processName' : 'DynamoDB Checker'},
+    dag=dag
+)
+maintask >> [t1 , checkDB ]
 t1 >>create_bucket >> t2 
+[checkDB , t2] >> t3
