148ef4276662947a13ec75831593e63dfe1f09bf
kmth-hash <bpk.san4ya@gmail.com>
2024-06-23 20:12:25 +0530

    Filters updated

--- src/enrichment/mapping.py
+++ src/enrichment/mapping.py
@@ -13,13 +13,17 @@ def preprocess(spark , ASOFDATE , logger ):
     userDF = readFile(spark , logger , r'src/enrichment/names.csv')
     userDF = userDF.select(col('Gender') , col("Child's First Name")).withColumnRenamed("Child's First Name",'userName')
     userDF.dropDuplicates(['userName'])
-
+    logger.info(f'Total Users found : {userDF.count()}')
+    userDF = userDF.dropDuplicates(['userName'])
+    logger.info(f'Distinct Users found : {userDF.count()}')
+    
     userDF = addStatustoDF(userDF)
     userDF = addStatustoDF(userDF) 
     userDF = addMedstoDF(userDF)
     userDF = addMedCounttoDF(userDF)
     
     # userDF.show()
+    userDF = userDF.withColumn("vaccinated" , when(col('medCount')>0 , "Vaccinated").otherwise("Not Vaccinated"))
     
     sampleDF = locationDF.crossJoin(userDF).sample(0.2)
     
@@ -35,19 +39,26 @@ def preprocess(spark , ASOFDATE , logger ):
 def countryStats(spark , logger , ASOFDATE , srcDF ) : 
     logger.info("Source Data loaded : ")
     logger.info(f"Total Source count for users : {srcDF.count()}")
-    logger.info(srcDF.show())
+    
     groupedDF = srcDF.groupby("country").pivot('status').count()
+
+    vacGroupDF = srcDF.groupby('country').pivot('vaccinated').count()
+    vacGroupDF = vacGroupDF.withColumnRenamed('country' , 'vacCountry')
+    # vacGroupDF.show(5)
+    # print(vacGroupDF.count())
+    # exit(0)
     srcDF = srcDF.drop("Gender" , "userName")
     srcDF = srcDF.withColumnRenamed('country' , 'srcCountry')
     srcDF = srcDF.dropDuplicates(['srcCountry'])
-    logger.info(srcDF.show())
-    srcDF = srcDF.join(groupedDF , groupedDF['country']==srcDF['srcCountry'] , 'inner')
-    srcDF = srcDF.withColumn("vaccinated" , when(col('medCount')>0 , "Vaccinated").otherwise("Not Vaccinated"))
-    print(srcDF.printSchema())
     
-    srcDF = srcDF.distinct().drop('medCount' , 'srcCountry', 'med' , 'status')
+    srcDF = srcDF.join(groupedDF , groupedDF['country']==srcDF['srcCountry'] , 'right').join(vacGroupDF , vacGroupDF['vacCountry']==srcDF['srcCountry'],'left' )
+    # srcDF.show(5)
+    # print(srcDF.printSchema())
     
-    srcDF.show()
+    srcDF = srcDF.distinct().drop('medCount' , 'srcCountry', 'vacCountry','med' , 'status')
+    logger.info(f'Distinct records found : {srcDF.count()}')
+    # logger.info(srcDF.collect())
+    # srcDF.show()
     return srcDF 
 
 
